8bb89bacd0bad9454a0d2a51de5e4e769c15e74d
Katy <katybucsa@yahoo.com>
2023-10-24 22:44:39 +0300

    add login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You are successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'There is something wrong with the login information'
+      render action: :new, status: :unprocessable_entity
+    end
   end
 end
