be1679b06cdc81a7610465b717cee6f561bac983
Katy <katybucsa@yahoo.com>
2023-10-25 00:17:19 +0300

    add message methods

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,6 +2,7 @@ class ChatroomController < ApplicationController
   before_action :require_user
 
   def index
+    @message = Message.new
     @messages = Message.includes(:user).all.order(:created_at)
   end
 end
--- app/javascript/controllers/chatroom_controller.js
+++ app/javascript/controllers/chatroom_controller.js
@@ -3,8 +3,13 @@ import {Controller} from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     $('.ui.dropdown').dropdown()
+
     $('.message .close').on('click', function () {
       $(this).closest('.message').transition('fade');
+    });
+
+    $('.send_message').on('click', function () {
+      $(this).closest('form').submit()
     });
   }
 }
