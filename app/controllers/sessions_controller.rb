fe67b2853c5b78ea8a21b7d989504bfc5cd1b294
Katy <katybucsa@yahoo.com>
2023-10-24 22:58:15 +0300

    add logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
       render action: :new, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to login_path
+  end
 end
