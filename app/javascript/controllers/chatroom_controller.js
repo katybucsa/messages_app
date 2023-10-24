57e290947fac2eb0e28d0d85c4576a383eb0a231
Katy <katybucsa@yahoo.com>
2023-10-24 23:05:23 +0300

    display error messages

--- app/javascript/controllers/chatroom_controller.js
+++ app/javascript/controllers/chatroom_controller.js
@@ -1,7 +1,10 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    connect() {
-        $('.ui.dropdown').dropdown()
-    }
+  connect() {
+    $('.ui.dropdown').dropdown()
+    $('.message .close').on('click', function () {
+      $(this).closest('.message').transition('fade');
+    });
+  }
 }
