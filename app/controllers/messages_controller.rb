1439e91bdc3f21e31941ff1935e2645acbca9059
Katy <katybucsa@yahoo.com>
2023-10-28 19:30:01 +0300

    show new message when added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.messages.build(message_params)
-    redirect_to root_path if message.save
+    ActionCable.server.broadcast 'chatroom_channel', { new_message: message.body } if message.save
   end
 
   private
